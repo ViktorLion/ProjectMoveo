abece4147fc6322443bb931f819b1dd5e49bbbdb
Viktor <lionvityalion@gmail.com>
2024-03-24 00:49:58 +0200

    Update webSocket.js

--- project-moveo/src/services/webSocket.js
+++ project-moveo/src/services/webSocket.js
@@ -2,7 +2,7 @@ let socket = null;
 
 
 const openSocket = (id) => {
-  socket = new WebSocket('ws://moveo-backend.vercel.app:8080');
+  socket = new WebSocket('ws://movero-backend-express-production.up.railway.app:8080');
   socket.onopen = () => {
     socket.send(JSON.stringify({ type: 'id', id }));
   };
